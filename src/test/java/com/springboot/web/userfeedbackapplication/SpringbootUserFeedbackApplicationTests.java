fd18f6d76f39e988099c8acb6efd65fab7d5a417
rakshashetty23 <rakshashetty680@gmail.com>
2020-07-08 20:34:25 +0530

    initial commit

--- src/test/java/com/springboot/web/userfeedbackapplication/SpringbootUserFeedbackApplicationTests.java
+++ src/test/java/com/springboot/web/userfeedbackapplication/SpringbootUserFeedbackApplicationTests.java
@@ -1,4 +1,4 @@
-package com.springboot.web.springbootfirstwebapp;
+package com.springboot.web.userfeedbackapplication;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringbootFirstwebappApplicationTests {
+public class SpringbootUserFeedbackApplicationTests {
 
 	@Test
 	public void contextLoads() {
